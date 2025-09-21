973a1c62a4e0233da2302b2ffbcd1d00ea6c4f35
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-21 23:57:18 +0900

    Feat: 커스텀 User 모델 필드 admin에 정의

--- capstone2/users/admin.py
+++ capstone2/users/admin.py
@@ -5,4 +5,10 @@ from users.models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = [
+        (None, {'fields': ('username', 'password')}),
+        ("Personal info", {'fields': ('first_name', 'last_name', 'email')}),
+        ("Additional fields", {'fields': ('short_description',)}),
+        ("Permissions", {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+        ("Important schedule",{'fields': ('last_login', 'date_joined')}),
+    ]
