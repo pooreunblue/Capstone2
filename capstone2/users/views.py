dabfc9190c7b8447a4e3c7574dd61d502ceb5ca1
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-23 16:27:18 +0900

    Style: 코드 개행 삭제

--- capstone2/users/views.py
+++ capstone2/users/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from users.models import User
 from users.serializers import UserSignUpSerializer
 
-
 class SignUpView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSignUpSerializer
