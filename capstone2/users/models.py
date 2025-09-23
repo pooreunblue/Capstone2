1ecf85a45a9e13d3c0ba9115d0c8ca316b7e62e1
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-23 09:30:53 +0900

    Feat: User 모델 필드 수정

--- capstone2/users/models.py
+++ capstone2/users/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    short_description = models.TextField("소개글", blank=True)
+    phone_number = models.CharField("전화번호", max_length=11, unique=True, blank=True)
+    email = models.EmailField("이메일", unique=True, blank=True)
     REQUIRED_FIELDS = ['email']
 
     def __str__(self):
