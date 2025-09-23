1bc2a6f523d3be6750b6cea60acd3766ec131478
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-23 16:27:42 +0900

    Refactor: admin.py 필드 수정

--- capstone2/users/admin.py
+++ capstone2/users/admin.py
@@ -7,8 +7,7 @@ from users.models import User
 class CustomUserAdmin(UserAdmin):
     fieldsets = [
         (None, {'fields': ('username', 'password')}),
-        ("Personal info", {'fields': ('first_name', 'last_name', 'email')}),
-        ("Additional fields", {'fields': ('short_description',)}),
+        ("Personal info", {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
         ("Permissions", {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
         ("Important schedule",{'fields': ('last_login', 'date_joined')}),
     ]
