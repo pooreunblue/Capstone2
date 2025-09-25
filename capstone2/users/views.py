07f8387507d5761e2fa9364064e1a9878e16f888
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-25 21:13:41 +0900

    Feat: 인증된 사용자만 접근할 수 있는 Feed view 추가

--- capstone2/users/views.py
+++ capstone2/users/views.py
@@ -1,7 +1,16 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from users.models import User
 from users.serializers import UserSignUpSerializer
 
 class SignUpView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSignUpSerializer
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'message':f'Hello {request.user.username}'})
