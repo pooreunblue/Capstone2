b6833e07ee5e2e7283867e652eb464646144224d
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-25 16:57:44 +0900

    Feat: 회원가입/로그인/토큰재발행 url 설정

--- capstone2/users/urls.py
+++ capstone2/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from config.urls import urlpatterns
 from users.views import SignUpView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
