0876f1cc113828381df5622f2b9d0beca2429d7c
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-26 16:13:37 +0900

    Refactor: username, email 직접 필드 정의, phone_number None 처리

--- capstone2/users/serializers.py
+++ capstone2/users/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from users.models import User
 
 class UserSignUpSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True,
+        validators=[
+            UniqueValidator(queryset=User.objects.all(), message="이미 사용 중인 아이디입니다. 다른 아이디를 입력해주세요.")
+        ],
+    )
+
+    email = serializers.EmailField(
+        required=True,
+        validators=[
+            UniqueValidator(queryset=User.objects.all(), message="이미 가입된 이메일입니다. 다른 이메일을 입력해주세요.")
+        ]
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'phone_number']
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {
+                'write_only': True
+            },
         }
 
     def create(self, validated_data):
@@ -14,6 +31,6 @@ class UserSignUpSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            phone_number=validated_data['phone_number']
+            phone_number=validated_data.get('phone_number', None)
         )
         return user
