6c982ad2bbdf2b2367cefc4a0023876f95a25633
pooreunblue <mjbsh02@mju.ac.kr>
2025-09-28 21:59:33 +0900

    Refactor: 인증 예외 처리

--- capstone2/users/views.py
+++ capstone2/users/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
 from users.models import User
 from users.serializers import UserSignUpSerializer
 
 class SignUpView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSignUpSerializer
+    permission_classes = [AllowAny]
