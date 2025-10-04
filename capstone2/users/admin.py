f17b5ba5526f51269b3bc9f0f54a7018b343b87f
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-04 17:44:45 +0900

    Feat: DormInfo, Profile admin 구현

--- capstone2/users/admin.py
+++ capstone2/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
 
-from users.models import User
+from users.models import User, DormInfo, Profile
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
@@ -10,4 +11,18 @@ class CustomUserAdmin(UserAdmin):
         ("Personal info", {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
         ("Permissions", {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
         ("Important schedule",{'fields': ('last_login', 'date_joined')}),
+    ]
+
+@admin.register(DormInfo)
+class DormInfoAdmin(ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ('user', 'name', 'sex')}),
+        ("Application info", {'fields': ('application_order', 'building', 'room', 'residency_period', 'is_accepted')}),
+    ]
+
+@admin.register(Profile)
+class ProfileAdmin(ModelAdmin):
+    fieldsets = [
+        (None, {'fields': 'user'}),
+        ("Profile info", {'fields': ('is_smoker', 'has_snoring', 'lifestyle_pattern', 'eat_in_room')}),
     ]
