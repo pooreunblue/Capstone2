bcc05a8e5cd82d04dc03acd7376599e981295201
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-08 12:33:00 +0900

    Feat: DormInfoSerializer 구현

--- capstone2/users/serializers.py
+++ capstone2/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from users.models import User
+from users.models import User, DormInfo
 
 class UserSignUpSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, required=True)
@@ -40,3 +40,28 @@ class UserSignUpSerializer(serializers.ModelSerializer):
             phone_number=validated_data.get('phone_number', None)
         )
         return user
+
+class DormInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DormInfo
+        fields = ['name', 'sex', 'application_order', 'building', 'room', 'residency_period', 'is_accepted']
+
+    def validate(self, data):
+        sex = data.get('sex')
+        building = data.get('building')
+        if sex == DormInfo.SexChoices.FEMALE and building == DormInfo.BuildingChoices.DONG_3:
+            raise serializers.ValidationError(
+                {"building": "여학생은 3동에 배정될 수 없습니다."}
+            )
+        male_restricted_buildings = [
+            DormInfo.BuildingChoices.MYEONGHYEON,
+            DormInfo.BuildingChoices.DONG_4,
+            DormInfo.BuildingChoices.DONG_5,
+        ]
+        if sex == DormInfo.SexChoices.MALE and building in male_restricted_buildings:
+            raise serializers.ValidationError(
+                {"building": "남학생은" + building + "에 배정될 수 없습니다."}
+            )
+        if data.get('is_accepted') != DormInfo.AcceptanceChoices.ACCEPTED:
+            raise serializers.ValidationError({"is_accepted": "기숙사 선발 정보가 확인되지 않습니다."})
+        return data
