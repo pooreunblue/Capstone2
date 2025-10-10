9027d5deca3ffd80b92efb0583c6e24052034a3d
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-10 16:31:56 +0900

    Feat: Profile 필드 추가, 수정

--- capstone2/users/models.py
+++ capstone2/users/models.py
@@ -57,8 +57,9 @@ class Profile(models.Model):
     class SmokingTypeChoices(models.TextChoices):
         NON_SMOKER = 'NON_SMOKER', '비흡연자'
         CIGARETTE = 'CIGARETTE', '연초'
-        E_CIGARETTE = 'E_CIGARETTE', '궐련형 전자담배'
-        LIQUID = 'LIQUID', '액상형 전자담배'
+        HEATED_TOBACCO_PRODUCT = 'HEATED_TOBACCO_PRODUCT', '궐련형 전자담배(릴, 아이코스, 글로 등)'
+        E_CIGARETTE = 'LIQUID_TYPE_E_CIGARETTE', '액상형 전자담배'
+        ETC = 'ETC', '기타'
         MIXED = 'MIXED', '두 종류 이상 혼합 흡연'
 
     class SmokingAmountChoices(models.TextChoices):
@@ -73,13 +74,15 @@ class Profile(models.Model):
         TALKING = 'TALKING', '잠꼬대'
 
     class SleepingHabitFreqChoices(models.TextChoices):
-        EVERYTIME = 'EVERYTIME', '매번',
+        EVERYTIME = 'EVERYTIME', '매번'
+        OFTEN = 'OFTEN', '자주'
+        SOMETIMES = 'SOMETIMES', '가끔'
         ONLY_WHEN_TIRED = 'ONLY_WHEN_TIRED', '피곤할 때만'
 
     class SleepingHabitExtentChoices(models.TextChoices):
-        WEAK = 'WEAK', '약함'
+        WEAK = 'WEAK', '낮고 약함(거의 들리지 않음)'
         NORMAL = 'NORMAL', '보통'
-        SEVERE = 'SEVERE', '심함'
+        SEVERE = 'SEVERE', '심함(소리가 커 수면에 방해될 수 있음)'
 
     class LifeStyleChoices(models.TextChoices):
         MORNING = 'MORNING', '아침형'
@@ -103,6 +106,17 @@ class Profile(models.Model):
         AFTER_TWO = 'AFTER_TWO', '2시 이후'
         FLEXIBLE = 'FLEXIBLE', '유동적'
 
+    class PreSleepingLifeStyleChoices(models.TextChoices):
+        RIGHT_AWAY = 'RIGHT_AWAY', '불 끄고 바로 잠드는 편'
+        USE_PHONE_LAPTOP_WITH_LIGHT = 'USE_PHONE_LAPTOP_WITH_LIGHT', '불 켜둔 채로 휴대폰/노트북 사용'
+        WITH_MUSIC_AND_VIDEOS = 'WITH_MUSIC_AND_VIDEOS', '음악, 영상 등 틀어놓고 잠듦'
+        FLEXIBLE = 'FLEXIBLE', '유동적'
+
+    class SensitivityToSleep(models.TextChoices):
+        INSENSITIVE = 'INSENSITIVE', '소리/불빛 있어도 괜찮음'
+        LITTLE_BIT_IS_FINE = 'LITTLE_BIT_IS_FINE', '약간의 불빛/소음은 괜찮음'
+        SENSITIVE = 'SENSITIVE','완전히 어둡고 조용해야 함'
+
     class CleaningCycleChoices(models.TextChoices):
         EVERYDAY = 'EVERYDAY', '매일'
         TWICE_A_WEEK = 'TWICE_A_WEEK', '한 주에 두 번'
@@ -111,11 +125,11 @@ class Profile(models.Model):
         RARELY = 'RARELY', '거의 하지 않음'
 
     class EatingInRoomChoices(models.TextChoices):
-        NEVER = 'NEVER', '절대 안 함'
-        SOMETIMES = 'SOMETIMES', '가끔'
-        OFTEN = 'OFTEN', '자주'
+        RARELY = 'RARELY', '거의 하지 않음'
+        SOMETIMES = 'SOMETIMES', '가끔 하는 편'
+        OFTEN = 'OFTEN', '자주 하는 편'
 
-    smoking_type = models.CharField("흡연 종류", max_length=20, choices=SmokingTypeChoices.choices, blank=True)
+    smoking_type = models.CharField("흡연 종류", max_length=25, choices=SmokingTypeChoices.choices, blank=True)
     smoking_amount = models.CharField("흡연량", max_length=20, choices=SmokingAmountChoices.choices, blank=True, null=True)
     sleeping_habit = models.CharField("잠버릇 종류", max_length=20, choices=SleepingHabitChoices.choices, blank=True)
     sleeping_habit_freq = models.CharField("잠버릇 빈도", max_length=20, choices=SleepingHabitFreqChoices.choices, blank=True, null=True)
@@ -123,6 +137,8 @@ class Profile(models.Model):
     life_style = models.CharField("생활 스타일", max_length=20, choices=LifeStyleChoices.choices, blank=True)
     wake_up_time = models.CharField("기상 시간", max_length=20, choices=WakeUptimeChoices.choices, blank=True)
     bed_time = models.CharField("취침 시간", max_length=20, choices=BedTimeChoices.choices, blank=True)
+    pre_sleeping_life_style = models.CharField("잠자기 전 생활습관", max_length=30, choices=PreSleepingLifeStyleChoices.choices, blank=True)
+    sensitivity_to_sleep = models.CharField("잠들 때 민감도", max_length=20, choices=SensitivityToSleep.choices, blank=True)
     cleaning_cycle = models.CharField("청소 주기", max_length=20, choices=CleaningCycleChoices.choices, blank=True)
     eating_in_room = models.CharField("실내 취식 여부", max_length=20, choices=EatingInRoomChoices.choices, blank=True)
 
