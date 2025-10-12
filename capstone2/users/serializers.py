ead7bc5e9ddde5cacae00dae65a367a47069b2a8
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-13 03:51:08 +0900

    Refactor: User Serializer 수정

--- capstone2/users/serializers.py
+++ capstone2/users/serializers.py
@@ -4,42 +4,24 @@ from users.models import User, DormInfo, Profile
 
 
 class UserSignUpSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(write_only=True, required=True)
-    username = serializers.CharField(
+    nickname = serializers.CharField(
         required=True,
         validators=[
-            UniqueValidator(queryset=User.objects.all(), message="이미 사용 중인 아이디입니다.")
+            UniqueValidator(queryset=User.objects.all(), message="이미 사용 중인 닉네임입니다. 다른 닉네임을 입력해주세요.")
         ],
-    )
-    email = serializers.EmailField(
-        required=True,
-        validators=[
-            UniqueValidator(queryset=User.objects.all(), message="이미 가입된 이메일입니다.")
-        ]
     )
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'phone_number']
-        extra_kwargs = {
-            'password': {
-                'write_only': True
-            },
-        }
-
-    def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError({"password": "비밀번호가 일치하지 않습니다."})
-        return data
+        fields = ['nickname', 'age']
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            phone_number=validated_data.get('phone_number', None)
+        user = User.objects.create(
+            nickname=validated_data['nickname'],
+            age=validated_data.get('age')
         )
+        user.set_unusable_password()
+        user.save()
         return user
 
 class DormInfoSerializer(serializers.ModelSerializer):
@@ -72,5 +54,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         exclude = ('user',)
 
-class DormAcceptImageSerializer(serializers.Serializer):
-    image = serializers.ImageField()
+class DormVerificationSerializer(serializers.Serializer):
+    image = serializers.ImageField(required=False)
