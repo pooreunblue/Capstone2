6943b426944d6582f77385636c8f21705d69fd59
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-14 11:22:16 +0900

    Refactor: User, DormInfo, Profile Admin 수정

--- capstone2/users/admin.py
+++ capstone2/users/admin.py
@@ -6,23 +6,23 @@ from users.models import User, DormInfo, Profile
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('nickname', 'age', 'is_staff')
+    list_display = ('nickname', 'application_order', 'is_staff')
     ordering = ('nickname', )
 
     fieldsets = [
-        (None, {"fields": ("nickname", "age", 'is_staff')}),
+        (None, {"fields": ("nickname", 'application_order', 'is_staff')}),
     ]
 
 @admin.register(DormInfo)
 class DormInfoAdmin(ModelAdmin):
     fieldsets = [
         (None, {'fields': ('user', 'student_id', 'name', 'sex')}),
-        ("Application info", {'fields': ('application_order', 'building', 'room', 'residency_period', 'selected_semester','is_accepted')}),
+        ("Application info", {'fields': ('building', 'room', 'residency_period', 'selected_semester','is_accepted')}),
     ]
 
 @admin.register(Profile)
 class ProfileAdmin(ModelAdmin):
     fieldsets = [
-        (None, {'fields': ('user',)}),
+        (None, {'fields': ('user', 'age', 'grade')}),
         ("Profile info", {'fields': ('smoking_type', 'smoking_amount', 'sleeping_habit', 'sleeping_habit_freq', 'sleeping_habit_extent', 'life_style', 'wake_up_time', 'bed_time', 'pre_sleeping_life_style', 'sensitivity_to_sleep', 'cleaning_cycle', 'eating_in_room')}),
     ]
