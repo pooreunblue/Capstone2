c7a84aeb70c48b7cb39882d3759ccdbb738328fb
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-14 13:46:01 +0900

    Refactor: Url 경로, 네이밍 수정

--- capstone2/users/urls.py
+++ capstone2/users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from users.views import SignUpView, DormInfoView, ProfileView
+from users.views import SignUpView, DormVerificationView, ProfileView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('dorminfo/', DormInfoView.as_view(), name='dorm-info'),
+    path('verify-dorm/', DormVerificationView.as_view(), name='verify-dorm'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
