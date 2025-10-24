a5ab5f7bc6366b8ff3cb2097ff63cdebc9269cfb
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-24 19:28:53 +0900

    Style: 에러 출력문 수정

--- capstone2/users/serializers.py
+++ capstone2/users/serializers.py
@@ -11,12 +11,10 @@ class DormVerificationSerializer(serializers.Serializer):
 
     def validate(self, data):
         image_file = data['image']
-
         temp_file_path = f'temp_{uuid.uuid4()}.jpg'
         with open(temp_file_path, 'wb+') as temp_file:
             for chunk in image_file.chunks():
                 temp_file.write(chunk)
-
         try:
             ocr_result = call_clova_ocr(temp_file_path)
             if not ocr_result.get("success"):
@@ -34,16 +32,16 @@ class DormVerificationSerializer(serializers.Serializer):
         building_text = ocr_data.get('dormitory_name', "")
         room_text = ocr_data.get('room_type', "")
         period_text = ocr_data.get('residency_period')
-        current_semester = "25-2학기"
+        current_semester = "25-1학기"
 
         if not student_id or DormInfo.objects.filter(student_id=student_id).exists():
-            raise serializers.ValidationError("이미 가입된 학번이거나, 학번을 인식할 수 없습니다.")
+            raise serializers.ValidationError({"image":"이미 가입된 학번이거나, 학번을 인식할 수 없습니다."})
 
         if selected_semester != current_semester:
-            raise serializers.ValidationError("현재 학기 합격자 조회결과가 아닙니다.")
+            raise serializers.ValidationError({"image":"현재 학기 합격자 조회 결과가 아닙니다."})
 
         if is_accepted_text != "선발":
-            raise serializers.ValidationError("기숙사 선발 대상자가 아닙니다.")
+            raise serializers.ValidationError({"image":"기숙사 선발 대상자가 아닙니다."})
 
         sex_enum = "MALE" if sex_text == "남자" else "FEMALE"
         if building_text == "명덕관":
@@ -57,17 +55,17 @@ class DormVerificationSerializer(serializers.Serializer):
         elif building_text == "5동":
             building_enum = "DONG_5"
         else:
-            raise serializers.ValidationError(f"지원 건물을 인식할 수 없습니다: {building_text}")
+            raise serializers.ValidationError({"image":"지원 건물을 인식할 수 없습니다."})
         accepted_enum = "ACCEPTED" if is_accepted_text == "선발" else "NOT_ACCEPTED"
         room_enum = "QUAD" if room_text == "4인실" else "DOUBLE"
         period_enum = "SEMESTER" if period_text == "학기" else "SIXMONTHS"
 
         if sex_enum == "FEMALE" and building_enum == "DONG_3":
-            raise serializers.ValidationError("여학생은 3동에 배정될 수 없습니다.")
+            raise serializers.ValidationError({"image":"여학생은 3동에 배정될 수 없습니다."})
 
         male_restricted = ['MYEONGHYEON', 'DONG_4', 'DONG_5']
         if sex_enum == "MALE" and building_enum in male_restricted:
-            raise serializers.ValidationError("남학생은" + building_text + "에 배정될 수 없습니다.")
+            raise serializers.ValidationError({"image":"남학생은 해당 건물에 배정될 수 없습니다."})
 
         validated_dorm_data = {
             "student_id": student_id,
