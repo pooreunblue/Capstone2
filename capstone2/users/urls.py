441f5fae03cdecc6c3818c5dbba139e6dae7dc50
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-24 19:22:42 +0900

    Feat: Token Refresh url 추가

--- capstone2/users/urls.py
+++ capstone2/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from users.views import SignUpView, DormVerificationView, ProfileView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('verify-dorm/', DormVerificationView.as_view(), name='verify-dorm'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
