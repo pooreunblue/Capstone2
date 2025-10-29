1cfe6dadf467c901269f9038c6c6a2b99465980e
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-29 09:29:47 +0900

    Refactor: config url 구성 수정

--- capstone2/config/urls.py
+++ capstone2/config/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
-    path("feeds/", include("feeds.urls")),
 ]
