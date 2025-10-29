bc6d4e51056e62bed7b53b281a027c771deb6d00
pooreunblue <mjbsh02@mju.ac.kr>
2025-10-29 09:35:03 +0900

    Refactor: URL TokenRefresh 경로 삭제 및 매칭 엔드포인트 추가

--- capstone2/users/urls.py
+++ capstone2/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from users.views import SignUpView, DormVerificationView, ProfileView
+from users.views import SignUpView, DormVerificationView, ProfileView, MatchingFeedView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('verify-dorm/', DormVerificationView.as_view(), name='verify-dorm'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('matching/', MatchingFeedView.as_view(), name='matching-feed'),
 ]
