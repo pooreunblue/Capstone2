c28906042c954cd0a8ccb57bc0fa4a5f141f1a96
pooreunblue <mjbsh02@mju.ac.kr>
2025-11-02 22:41:10 +0900

    Feat: 다른 사용자 프로필 조회하는 Url 구현

--- capstone2/users/urls.py
+++ capstone2/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import SignUpView, DormVerificationView, ProfileView, MatchingFeedView
+from users.views import SignUpView, DormVerificationView, ProfileView, MatchingFeedView, UserProfileDetailView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('verify-dorm/', DormVerificationView.as_view(), name='verify-dorm'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('matching/', MatchingFeedView.as_view(), name='matching-feed'),
+    path('profile/<int:user_id>/', UserProfileDetailView.as_view(), name='user-profile-detail'),
 ]
