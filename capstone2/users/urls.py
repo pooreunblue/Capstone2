27c206bfd96bd6aa3f6fc87232854793d40d41a7
pooreunblue <mjbsh02@mju.ac.kr>
2025-11-04 01:37:27 +0900

    Feat: 마이페이지 url 추가

--- capstone2/users/urls.py
+++ capstone2/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from users.views import SignUpView, DormVerificationView, ProfileView, MatchingFeedView, UserProfileDetailView
+from users.views import SignUpView, DormVerificationView, ProfileView, MatchingFeedView, UserProfileDetailView, MyPageView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('verify-dorm/', DormVerificationView.as_view(), name='verify-dorm'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/', ProfileView.as_view(), name='profile'), # 초기 내 프로필 생성/수정
     path('matching/', MatchingFeedView.as_view(), name='matching-feed'),
-    path('profile/<int:user_id>/', UserProfileDetailView.as_view(), name='user-profile-detail'),
+    path('profile/<int:user_id>/', UserProfileDetailView.as_view(), name='user-profile-detail'), # 타인 프로필
+    path('users/me/', MyPageView.as_view(), name='my-page'), # 마이페이지
 ]
