5f65e14bfee64cd5287713366d5a90e92ac3580f
pooreunblue <mjbsh02@mju.ac.kr>
2025-11-05 23:34:54 +0900

    Feat: 쪽지 모델 구현 및 추가

--- capstone2/users/models.py
+++ capstone2/users/models.py
@@ -154,3 +154,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.nickname}님의 프로필"
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
