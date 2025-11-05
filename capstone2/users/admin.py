3ced5dab2bf9e611b4dd8ac634e481a97948527b
pooreunblue <mjbsh02@mju.ac.kr>
2025-11-05 23:15:53 +0900

    Feat: admin.py 사용자 추가 필드셋 설정

--- capstone2/users/admin.py
+++ capstone2/users/admin.py
@@ -4,25 +4,23 @@ from django.contrib.auth.admin import UserAdmin
 
 from users.models import User, DormInfo, Profile
 
-@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ('nickname', 'application_order', 'is_staff')
     ordering = ('nickname', )
+    search_fields = ('nickname',)
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('nickname', 'application_order'),
+        }),
+    )
 
     fieldsets = [
         (None, {"fields": ("nickname", 'application_order', 'is_staff')}),
     ]
 
-@admin.register(DormInfo)
-class DormInfoAdmin(ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ('user', 'student_id', 'name', 'sex')}),
-        ("Application info", {'fields': ('building', 'room', 'residency_period', 'selected_semester','is_accepted')}),
-    ]
+admin.site.register(User, CustomUserAdmin)
 
-@admin.register(Profile)
-class ProfileAdmin(ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ('user', 'age', 'grade')}),
-        ("Profile info", {'fields': ('smoking_type', 'smoking_amount', 'sleeping_habit', 'sleeping_habit_freq', 'sleeping_habit_extent', 'life_style', 'wake_up_time', 'bed_time', 'pre_sleeping_life_style', 'sensitivity_to_sleep', 'cleaning_cycle', 'eating_in_room')}),
-    ]
+admin.site.register(DormInfo)
+admin.site.register(Profile)
