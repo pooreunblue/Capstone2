f64edc531fddc9e65b0d611f09a5f0205952c713
pooreunblue <mjbsh02@mju.ac.kr>
2025-11-06 10:07:24 +0900

    Feat: 메세지 전송 url 추가

--- capstone2/users/urls.py
+++ capstone2/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from users.views import SignUpView, DormVerificationView, ProfileView, MatchingFeedView, UserProfileDetailView, MyPageView
+from users.views import SignUpView, DormVerificationView, ProfileView, MatchingFeedView, UserProfileDetailView, MyPageView, MessageSendView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('matching/', MatchingFeedView.as_view(), name='matching-feed'),
     path('profile/<int:user_id>/', UserProfileDetailView.as_view(), name='user-profile-detail'), # 타인 프로필
     path('mypage/', MyPageView.as_view(), name='my-page'), # 마이페이지
+    path('messages/send/', MessageSendView.as_view(), name='message-send'), # 쪽지 보내기
 ]
