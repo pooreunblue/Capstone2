3f30a90d29be91f968f74216dc1dbc8ef36fb1d3
pooreunblue <mjbsh02@mju.ac.kr>
2025-11-06 11:10:36 +0900

    Feat: 대화 목록 Serializer 구현

--- capstone2/users/serializers.py
+++ capstone2/users/serializers.py
@@ -344,3 +344,24 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = ['id', 'sender', 'sender_nickname', 'recipient', 'recipient_nickname', 'content', 'timestamp', 'is_read']
         read_only_fields = ['id', 'sender', 'sender_nickname','recipient_nickname','timestamp', 'is_read']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    opponent_nickname = serializers.SerializerMethodField()
+    last_message = serializers.CharField(source='content')
+
+    class Meta:
+        model = Message
+        fields = [
+            'id', 'opponent_nickname', 'last_message', 'timestamp', 'is_read'
+        ]
+
+    def get_opponent_nickname(self, message):
+        request_user = self.context.get('request_user')
+
+        if not request_user:
+            return "알 수 없음"
+
+        if message.sender == request_user:
+            return message.recipient.nickname
+        else:
+            return message.sender.nickname
