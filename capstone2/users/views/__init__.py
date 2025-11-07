651b3ae5e26c542347075ea462185a3205f32cb0
pooreunblue <mjbsh02@mju.ac.kr>
2025-11-07 16:45:01 +0900

    Feat: ConversationDetailView 패키지 멤버 구성 추가

--- capstone2/users/views/__init__.py
+++ capstone2/users/views/__init__.py
@@ -1,10 +1,10 @@
 from .auth_views import DormVerificationView, SignUpView, ProfileView
 from .matching_views import MatchingFeedView, UserProfileDetailView
 from .mypage_views import  MyPageView
-from .messages_views import MessageSendView, ConversationListView
+from .messages_views import MessageSendView, ConversationListView, ConversationDetailView
 
 __all__ = [
     'DormVerificationView', 'SignUpView', 'ProfileView',
     'MatchingFeedView', 'UserProfileDetailView', 'MyPageView',
-    'MessageSendView', 'ConversationListView',
+    'MessageSendView', 'ConversationListView', 'ConversationDetailView',
 ]
